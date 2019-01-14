fc9985511e1f33ea4032e64061ef73f53fcf248b
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-01-14 14:30:37 -0500

    configured for qt-creator with sdl2 and sdl2-image (pending box2d)

--- sdl-game/main.cpp
+++ sdl-game/main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <SDL.h>
 #include <SDL_image.h>
-#include <Box2D/Box2D.h>
+//#include <Box2D/Box2D.h>
 #undef main
 
 #include "debug.h"
@@ -13,93 +13,93 @@
 
 int main(int argc, char *argv[])
 {
-	game_beginning: // label for debug game restart
+    game_beginning: // label for debug game restart
 
-	// init sdl
-	if (!g::init(g::window, g::windowSurface))
-	{
-		DEBUG_MSG("Init failed");
-		return -1;
-	}
+    // init sdl
+    if (!g::init(g::window, g::windowSurface))
+    {
+        DEBUG_MSG("Init failed");
+        return -1;
+    }
 
-	// construct player
-	playerObj *player = new playerObj(1, 1, 10, 100);
+    // construct player
+    playerObj *player = new playerObj(1, 1, 10, 100);
 
-	// list of all objects
-	std::vector<gameObj*> currentObjs;
+    // list of all objects
+    std::vector<gameObj*> currentObjs;
 
-	// right wall
-	gameObj *wall = new gameObj("hello_world.bmp", true, g::OBJ_BLOCK, g::SCREEN_WIDTH - 100, 0, 100, g::SCREEN_HEIGHT);
-	gameObj *block = new gameObj("hello_world.bmp", true, g::OBJ_BLOCK, g::SCREEN_WIDTH/4, g::SCREEN_HEIGHT/4, 300, 100);
-	gameObj *block2 = new gameObj("hello_world.bmp", true, g::OBJ_BLOCK, 0, g::SCREEN_HEIGHT - 100, g::SCREEN_WIDTH, 100);
+    // right wall
+    gameObj *wall = new gameObj("hello_world.bmp", true, g::OBJ_BLOCK, g::SCREEN_WIDTH - 100, 0, 100, g::SCREEN_HEIGHT);
+    gameObj *block = new gameObj("hello_world.bmp", true, g::OBJ_BLOCK, g::SCREEN_WIDTH/4, g::SCREEN_HEIGHT/4, 300, 100);
+    gameObj *block2 = new gameObj("hello_world.bmp", true, g::OBJ_BLOCK, 0, g::SCREEN_HEIGHT - 100, g::SCREEN_WIDTH, 100);
 
-	currentObjs.push_back(wall);
-	currentObjs.push_back(block);
-	currentObjs.push_back(block2);
+    currentObjs.push_back(wall);
+    currentObjs.push_back(block);
+    currentObjs.push_back(block2);
 
-	// game loop
-	//===========
-	while (!g::quit)
-	{
-		// event polling loop
-		while (SDL_PollEvent(&g::event))
-		{
-			// window close event
-			if (g::event.type == SDL_QUIT)
-			{
-				g::quit = true;
-				break;
-			}
-		}
+    // game loop
+    //===========
+    while (!g::quit)
+    {
+        // event polling loop
+        while (SDL_PollEvent(&g::event))
+        {
+            // window close event
+            if (g::event.type == SDL_QUIT)
+            {
+                g::quit = true;
+                break;
+            }
+        }
 
-		// check keystate
-		//===============
-		#ifdef DEBUG
-		if (g::keyState[SDL_SCANCODE_RETURN]) // DEBUG: quick quitting
-			g::quit = true;
+        // check keystate
+        //===============
+        #ifdef DEBUG
+        if (g::keyState[SDL_SCANCODE_RETURN]) // DEBUG: quick quitting
+            g::quit = true;
 
-		if (g::keyState[SDL_SCANCODE_R]) // DEBUG: restart
-		{
-			DEBUG_MSG("\n** restart **\n");
-			goto restart_label;
-		}
-		#endif
+        if (g::keyState[SDL_SCANCODE_R]) // DEBUG: restart
+        {
+            DEBUG_MSG("\n** restart **\n");
+            goto restart_label;
+        }
+        #endif
 
-		player->checkCollision(currentObjs); // check collision against current objs
+        player->checkCollision(currentObjs); // check collision against current objs
 
-		player->checkKeyState(); // check keys, update flags
-		player->updatePhysics(); // update player physics
+        player->checkKeyState(); // check keys, update flags
+        player->updatePhysics(); // update player physics
 
-		// render scene
-		// ============
+        // render scene
+        // ============
 		
-		// update window
-		SDL_SetRenderDrawColor(g::renderer, 0,0,0,0);
-		SDL_RenderClear(g::renderer);
+        // update window
+        SDL_SetRenderDrawColor(g::renderer, 0,0,0,0);
+        SDL_RenderClear(g::renderer);
 
-		// render player
-		SDL_RenderCopy(g::renderer, player->getCurrentTexture(), nullptr, &player->rect);
+        // render player
+        SDL_RenderCopy(g::renderer, player->getCurrentTexture(), nullptr, &player->rect);
 
-		// render all current objs
-		for(auto obj : currentObjs)
-			SDL_RenderCopy(g::renderer, obj->getCurrentTexture(), nullptr, &obj->rect);
+        // render all current objs
+        for(auto obj : currentObjs)
+            SDL_RenderCopy(g::renderer, obj->getCurrentTexture(), nullptr, &obj->rect);
 
-		SDL_RenderPresent(g::renderer);
+        SDL_RenderPresent(g::renderer);
 
-		SDL_Delay(16);
-	}
+        SDL_Delay(16);
+    }
 
-	//==============
-	// end game loop
+    //==============
+    // end game loop
 
-	// close SDL subsystems
-	g::close();
+    // close SDL subsystems
+    g::close();
 
-	return 0;
+    return 0;
 
-	#ifdef DEBUG
-	restart_label:
-	goto game_beginning;
-	#endif // DEBUG: game restarting
+    #ifdef DEBUG
+    restart_label:
+    goto game_beginning;
+    #endif // DEBUG: game restarting
 
 }
