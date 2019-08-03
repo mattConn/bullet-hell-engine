4423c3e2a37fa489a2546c7e0d10187ddf2c4804
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-08-03 16:58:54 -0400

    added get player input function

--- sdl-game/main.cpp
+++ sdl-game/main.cpp
@@ -10,6 +10,7 @@
 #include "bulletContainers.h"
 #include "animation.h"
 #include "gameObj.h"
+#include "getPlayerInput.h"
 
 
 int main(int argc, char* argv[])
@@ -102,37 +103,8 @@ int main(int argc, char* argv[])
 		// run when not paused
 		if (!paused)
 		{
-
-			// player keybindings
-			// ==================
-			// slow down
-			if (keyState[SDL_SCANCODE_LSHIFT])
-				player.setVelocityMod(.35);
-			else
-				player.setVelocityMod(1);
-
-			// fire
-			if (keyState[SDL_SCANCODE_Z] && SDL_TICKS_PASSED(SDL_GetTicks(), player.getBulletPtr()->getTimeout()))
-			{
-				currentPlayerBullets.push_back(player.getBulletCopy());
-				player.getBulletPtr()->resetTimeout();
-			}
-
-			// move left
-			if (keyState[SDL_SCANCODE_LEFT] && player.getRectL() > 0)
-				player.incRectX(-player.getVelocity() * player.getVelocityMod());
-
-			// move right
-			if (keyState[SDL_SCANCODE_RIGHT] && player.getRectR() < global::SCREEN_WIDTH)
-				player.incRectX(player.getVelocity() * player.getVelocityMod());
-
-			// move up
-			if (keyState[SDL_SCANCODE_UP] && player.getRectTop() > 0)
-				player.incRectY(-player.getVelocity() * player.getVelocityMod());
-
-			// move down
-			if (keyState[SDL_SCANCODE_DOWN] && player.getRectBottom() < global::SCREEN_HEIGHT)
-				player.incRectY(player.getVelocity() * player.getVelocityMod());
+			// get input
+			getPlayerInput(player, keyState);
 
 			// render scene
 			// ============
