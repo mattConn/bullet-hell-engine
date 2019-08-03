b3c8d73f80a57ab28aa6f05809aa3f6597a2e633
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-08-03 16:38:32 -0400

    removed gameObj.cpp from vcxproj (visual studio), changed screen size

--- sdl-game/global.cpp
+++ sdl-game/global.cpp
@@ -12,8 +12,8 @@ const Uint8 SCREEN_WINDOWED = 0;
 const Uint8 SCREEN_FULL = SDL_WINDOW_FULLSCREEN;
 Uint8 screenMode = SCREEN_WINDOWED;
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
 SDL_Window *window = nullptr; // main window
 SDL_Surface *windowSurface = nullptr; // surface for main window
