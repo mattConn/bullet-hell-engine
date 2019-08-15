1a324a171ac40c04c2f41ccf31eba62ea2a6bfa7
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-08-15 17:17:52 -0400

    defined baseObjs map in cpp file

---
+++ sdl-game/baseObjects.cpp
@@ -0,0 +1,26 @@
+#include <map>
+#include <string>
+
+#include "baseObjects.h"
+#include "animation.h"
+#include "gameObj.h"
+
+// base objects to copy
+
+std::map<std::string, gameObj*> baseBullets = {
+	{
+		"orange",
+		new gameObj("bullet-orange", 7, 20, 20, 500)
+	},
+
+	{
+		"red",
+		new gameObj("bullet-red", -10, 20, 20, 100)
+	}
+};
+std::map<std::string, gameObj*> baseEnemies = {
+	{
+		"bat",
+		new gameObj("enemy-bat", 8, 0, 0, 50, 46, baseBullets["orange"])
+	}
+};
--- sdl-game/enemyWaves.h
+++ sdl-game/enemyWaves.h
@@ -6,25 +6,6 @@
 #include "animation.h"
 #include "gameObj.h"
 
-// base objects to copy
-std::map<std::string, gameObj*> baseBullets = {
-	{
-		"orange",
-		new gameObj("bullet-orange", 7, 20, 20, 500)
-	},
-
-	{
-		"red",
-		new gameObj("bullet-red", -10, 20, 20, 100)
-	}
-};
-std::map<std::string, gameObj*> baseEnemies = {
-	{
-		"bat",
-		new gameObj("enemy-bat", 8, 0, 0, 50, 46, baseBullets["orange"])
-	}
-};
-
 std::vector<std::vector<gameObj*>> enemyWaves = {
 	// wave 1
 	{
